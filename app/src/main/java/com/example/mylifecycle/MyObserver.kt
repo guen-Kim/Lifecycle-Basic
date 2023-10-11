a657055643353b22c9d6387b7ba72afcff58a078
guen <guen5997@naver.com>
2023-10-11 23:50:19 +0900

    🔧wrench: Lifecycles

---
+++ app/src/main/java/com/example/mylifecycle/MyObserver.kt
@@ -0,0 +1,25 @@
+package com.example.mylifecycle
+
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+
+//
+class MyObserver : DefaultLifecycleObserver {
+
+    override fun onResume(owner: LifecycleOwner) {
+
+
+
+    }
+
+
+    override fun onPause(owner: LifecycleOwner) {
+        super.onPause(owner)
+
+    }
+
+
+
+
+
+}
