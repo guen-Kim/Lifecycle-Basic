f791a5f16d8ef0ce9512a1ac4bff821ac8805e11
guen <guen5997@naver.com>
2023-10-12 00:33:35 +0900

    ✨Feat: 관심사 분리 원칙, LifeCycleObserver - 수명주기 이벤트 처리

---
+++ app/src/main/java/com/example/mylifecycle/ExampleActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mylifecycle
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+class ExampleActivity : Activity(), LifecycleOwner {
+    private lateinit var lifecycleRegistry: LifecycleRegistry
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        lifecycleRegistry = LifecycleRegistry(this)
+
+        //Lifecycle 객체 획득
+        //addObserver를 사용하여 Owner에 Observer 추가
+        lifecycleRegistry.addObserver(MyObserver())
+    }
+
+    override fun getLifecycle(): Lifecycle {
+        return lifecycleRegistry
+    }
+}
--- app/src/main/java/com/example/mylifecycle/MyObserver.kt
+++ app/src/main/java/com/example/mylifecycle/MyObserver.kt
@@ -1,25 +1,42 @@
 package com.example.mylifecycle
 
+import android.util.Log
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 
-//
+// DefaultLifecycleObserver 인터페이스 구현
 class MyObserver : DefaultLifecycleObserver {
 
-    override fun onResume(owner: LifecycleOwner) {
-
-
-
+    // lifecycle callback 메서드 구현
+    override fun onCreate(owner: LifecycleOwner) {
+        super.onCreate(owner)
+        Log.d("MyLifecycle", "onCreate")
     }
 
+
+    override fun onResume(owner: LifecycleOwner) {
+        super.onCreate(owner)
+        Log.d("MyLifecycle", "onResume")
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+        Log.d("MyLifecycle", "onStart")
+    }
 
     override fun onPause(owner: LifecycleOwner) {
         super.onPause(owner)
-
+        Log.d("MyLifecycle", "onPause")
     }
 
+    override fun onStop(owner: LifecycleOwner) {
+        super.onStop(owner)
+        Log.d("MyLifecycle", "onStop")
+    }
 
+    override fun onDestroy(owner: LifecycleOwner) {
+        super.onDestroy(owner)
+        Log.d("MyLifecycle", "onDestroy")
 
-
-
+    }
 }
